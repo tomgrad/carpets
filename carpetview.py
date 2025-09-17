d08b16a819ca7774cdc80fca1b3fd814e00e88e9
Tomasz Gradowski <tomgrad@gmail.com>
2025-09-17 21:54:29 +0200

    add minor ticks to the bottom axis

--- carpetview.py
+++ carpetview.py
@@ -21,13 +21,14 @@ class CarpetView(pg.ImageView):
     def setXticks(self, left_off, right_off, sampling_rate, num=6, unit='ms'):
         
         if unit=='ms':
-            ticks = np.linspace(0, left_off+right_off, num)
+            majorTicks = np.linspace(0, left_off+right_off, num)
+            minorTicks = np.linspace(0, left_off+right_off, 5*(num-1)+1)
             self.view.getAxis('bottom').setTicks([
-            [(t.item(), str(int((t-sampling_rate)*1000/sampling_rate))+' ms') for t in ticks]
+            [(t.item(), str(int((t-sampling_rate)*1000/sampling_rate))+' ms') for t in majorTicks],
+            [(t.item(), '') for t in minorTicks]
             ])
         elif unit=='bpm':
             ticks = np.linspace(left_off, left_off+right_off, num+1)
-
             self.view.getAxis('bottom').setTicks([
             [(t.item(), str(int(60*sampling_rate/(t-sampling_rate)))) if t>sampling_rate else (t.item(), 'bpm') for t in ticks ]
             ])
